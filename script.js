fccc2076af6eaae4dbc296f4fba1667734a0518e
lexjacobs <lex.jacobs@gmail.com>
2014-11-11 09:27:21 -0800

    Connects button to click listener to play audio.

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 $(function() {
 
-        var playSound = function(soundfile) {
-            $('#sound-span').html('<embed src="' + soundfile +
-                '" hidden="true" autostart="true"' +
-                'loop="false">');
-        };
 
-            $('.button1').show();
-            $('.button2').hide();
+    $('.button1').on('click', function() {
+        var sound = $("#control")[0];
 
-            $('.container').on('click', function() {
+        sound.load();
+        sound.play();
+
+        return null;
+    });
+
+    $('.button1').show();
+    $('.button2').hide();
+
+    $('.container').on('click', function() {
+        $('.button1').toggle();
+        $('.button2').toggle();
+        $('#control').click();
+
+        setTimeout(
+            function() {
                 $('.button1').toggle();
                 $('.button2').toggle();
-                playSound('kittenmeow.wav');
-
-                setTimeout(
-                    function() {
-                        $('.button1').toggle();
-                        $('.button2').toggle();
-                    }, 200);
-            });
-        });
+            }, 300);
+    });
+});
