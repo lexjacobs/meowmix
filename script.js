fced03a9da2e26bf9fa8ede1debf72d362e3cfbb
lexjacobs <lex.jacobs@gmail.com>
2014-11-11 21:50:55 -0800

    Now 3 cats, 1 goat, title, fuschia background.

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 $(function() {
 
-        var sound = $("#control")[0];
-        sound.load();
+    var sound0 = $("#control0")[0];
+    sound0.load();
 
-    $('.button1').on('click', function() {
+    var sound1 = $("#control1")[0];
+    sound1.load();
 
-        sound.play();
+    var sound2 = $("#control2")[0];
+    sound2.load();
 
+    var sound3 = $("#control3")[0];
+    sound3.load();
+
+    $('.sticker0').on('click', function() {
+        sound0.play();
         return null;
     });
 
-    $('.button1').show();
-    $('.button2').hide();
-
-    $('.container').on('click', function() {
-        $('.button1').toggle();
-        $('.button2').toggle();
-        $('#control').click();
-
-        setTimeout(
-            function() {
-                $('.button1').toggle();
-                $('.button2').toggle();
-            }, 300);
+    $('.sticker1').on('click', function() {
+        sound1.play();
+        return null;
     });
+
+    $('.sticker2').on('click', function() {
+        sound2.play();
+        return null;
+    });
+
+    $('.sticker3').on('click', function() {
+        sound3.play();
+        return null;
+    });
+
+
 });
