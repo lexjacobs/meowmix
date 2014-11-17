fc4c80c8ede8f4ef8081a261e610e703690771a9
lexjacobs <lex.jacobs@gmail.com>
2014-11-16 20:56:47 -0800

    Sets initial volume > 0 to hopefully avoid initial loading delay upon start.

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ $(function() {
 
     var sound0 = $("#control0")[0];
     sound0.load();
-    sound0.volume = 0;
+    sound0.volume = 0.0005;
     sound0.play();
 
     var sound1 = $("#control1")[0];
     sound1.load();
-    sound1.volume = 0;
+    sound1.volume = 0.0005;
     sound1.play();
 
     var sound2 = $("#control2")[0];
     sound2.load();
-    sound2.volume = 0;
+    sound2.volume = 0.0005;
     sound2.play();
 
     var sound3 = $("#control3")[0];
     sound3.load();
-    sound3.volume = 0;
+    sound3.volume = 0.0005;
     sound3.play();
 
     $('.sticker0').on('click', function() {
